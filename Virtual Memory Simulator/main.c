63be42f7481c1fabca3634b50bfdd974ca863d07
Alexis Vu <alexis.lauren.vu@gmail.com>
2020-02-09 20:29:41 -0800

    Update.

--- Virtual Memory Simulator/main.c
+++ Virtual Memory Simulator/main.c
@@ -5,14 +5,13 @@
 
 #define NUM_ADDR_VM 32
 #define NUM_PAGE_VM 8
+#define NUM_PTE 8
 
-#define NUM_ADDR_MM 8
+#define NUM_ADDR_MM 16
 #define NUM_PAGE_MM 4
 
-#define NUM_ADDR_DISK 16
-#define NUM_PAGE_DISK 6
-
-#define NUM_PTE 8
+#define NUM_ADDR_DISK 32
+#define NUM_PAGE_DISK 8
 
 #define MAX_ARGS 3
 #define MAX_CHAR 50
@@ -29,6 +28,38 @@ typedef struct {
   int address;
   int data;
 } MemoryEntry;
+
+// MEMORY TABLES
+PageTableEntry pTable[NUM_PTE];     // page table
+MemoryEntry mainMem[NUM_ADDR_MM];   // main memory
+MemoryEntry disk[NUM_ADDR_DISK];    // disk memory
+
+void initialize() {
+  int i;
+  // initialize page table
+  for (i = 0; i < NUM_PTE; ++i) {
+    pTable[i].entry = i;
+    pTable[i].valid = 0;
+    pTable[i].dirty = 0;
+    pTable[i].pageNum = i;
+  }
+
+  // initialize main memory
+  int page = 0;
+  for (i = 0; i < NUM_ADDR_MM; ++i) {
+    mainMem[i].page = i % 2 == 0 ? page : page++;
+    mainMem[i].address = i;
+    mainMem[i].data = -1;
+  }
+
+  // initialize disk memory
+  page = 0;
+  for (i = 0; i < NUM_ADDR_DISK; ++i) {
+    disk[i].page = i % 4 == 0 ? page : page++;
+    disk[i].address = i;
+    disk[i].data = -1;
+  }
+}
 
 char** parse() {
   char* line = NULL;
@@ -56,10 +87,10 @@ char** parse() {
   return args;
 }
 
-void showPTable(PageTableEntry pt[]) {
+void showPTable() {
   int i;
   for (i = 0; i < NUM_PTE; ++i) {
-    printf("%d:%d:%d:%d\n", pt[i].entry, pt[i].valid, pt[i].dirty, pt[i].pageNum);
+    printf("%d:%d:%d:%d\n", pTable[i].entry, pTable[i].valid, pTable[i].dirty, pTable[i].pageNum);
   }
 }
 
@@ -71,43 +102,20 @@ void printMemory(MemoryEntry memory[], size_t size) {
   }
 }
 
-int main() {
-  PageTableEntry pTable[NUM_PTE];         // page table
-  MemoryEntry main[NUM_ADDR_MM];      // main memory
-  MemoryEntry disk[NUM_ADDR_DISK];    // disk memory
-  bool end = false;                   // flag for ending shell
-  char* *args = NULL;                 // user input
-  int i;                              // counter variable
-
-  // initialize page table
-  for (i = 0; i < NUM_PTE; ++i) {
-    pTable[i].entry = i;
-    pTable[i].valid = 0;
-    pTable[i].dirty = 0;
-    pTable[i].pageNum = i;
-  }
-
-  // initialize main memory
-  int page = 0;
-  for (i = 0; i < NUM_ADDR_MM; ++i) {
-    main[i].page = i % 2 == 1 ? page++ : page;
-    main[i].address = i;
-    main[i].data = -1;
-  }
-
-  // initialize disk memory
-  page = 0;
-  for (i = 0; i < NUM_ADDR_DISK; ++i) {
-    disk[i].page = i % 2 == 1 ? page++ : page;
-    disk[i].address = i;
-    disk[i].data = -1;
-  }
+int main(int argc, char* argv[]) {
+  bool end = false;     // flag for ending shell
+  char* *args = NULL;   // user input
+  int i;                // counter variable
 
   int vAddr;     // virtual address
   int vPage;     // virtual page
   int pAddr;     // physical address
   int pPage;     // physical page (main mem)
-  int offset;
+  int dPage;     // disk page
+  int dAddr;     // disk address
+  int offset;    // address offset from page start
+
+  initialize();
 
   while (!end) {
     // print prompt
@@ -128,7 +136,7 @@ int main() {
         }
         else {
           vAddr = atoi(args[1]);
-          vPage = vAddr >> 1;
+          vPage = vAddr >> 2;
           // printf("%d %d ", vAddr, vPage);
           pPage = pTable[vPage].pageNum;
 
@@ -148,13 +156,31 @@ int main() {
 
       }
       else if (strcmp(args[0], "showmain") == 0) {
-
+        if (args[1] == NULL) {
+          continue;
+        }
+        else {
+          pPage = atoi(args[1]);
+          pAddr = pPage * 4;
+          for (i = 0; i < 4; ++i) {
+            printf("%d:%d\n", mainMem[pAddr + i].address, mainMem[pAddr + i].data);
+          }
+        }
       }
       else if (strcmp(args[0], "showdisk") == 0) {
-
+        if (args[1] == NULL) {
+          continue;
+        }
+        else {
+          dPage = atoi(args[1]);
+          dAddr = dPage * 4;
+          for (i = 0; i < 4; ++i) {
+            printf("%d:%d\n", disk[dAddr + i].address, disk[dAddr + i].data);
+          }
+        }
       }
       else if (strcmp(args[0], "showptable") == 0) {
-        showPTable(pTable);
+        showPTable();
       }
     }
   }
