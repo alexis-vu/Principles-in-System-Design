2a04390488c4199dab257e343ad9d3559aa61303
Alexis Vu <alexis.lauren.vu@gmail.com>
2020-02-09 16:06:11 -0800

    Added parser and program branching.

--- Virtual Memory Simulator/main.c
+++ Virtual Memory Simulator/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
 
 #define NUM_ADDR_VM 32
@@ -7,6 +8,7 @@
 #define NUM_PAGE_VM 8
 #define NUM_PAGE_MM 4
 #define NUM_PTE 8
+
 #define MAX_ARGS 3
 #define MAX_CHAR 50
 
@@ -23,14 +25,55 @@ typedef struct {
   int data;
 } MainMemoryEntry;
 
-void parse(char, char*);
+char** parse() {
+  char* line = NULL;
+  size_t size = 0;
+  getline(&line, &size, stdin);
+
+  // if empty line, return null to skip and return prompt
+  if (strcmp(line, "\n") == 0) {
+    return NULL;
+  }
+
+  // otherwise, setup for tokenizing input line
+  char** args = (char**) malloc(MAX_ARGS * sizeof(char*));
+  char* arg = NULL;
+  int i = 0;
+
+  // get first arg, skip whitespace
+  arg = strtok(line, " \n\t\r\a");
+  while (arg != NULL) {
+    args[i] = arg;
+    ++i;
+    arg = strtok(NULL, " \n\t\r\a");
+  }
+
+  return args;
+}
+
+void showPTable(PageTableEntry pt[]) {
+  int i;
+  for (i = 0; i < NUM_PTE; ++i) {
+    printf("%d:%d:%d:%d\n", pt[i].entry, pt[i].valid, pt[i].dirty, pt[i].pageNum);
+  }
+}
+
+void printMain(MainMemoryEntry mainMem[]) {
+  int i;
+  printf("MAIN MEMORY:\n");
+  for (i = 0; i < NUM_ADDR_MM; ++i) {
+    printf("%i:%i:%i\n", mainMem[i].page, mainMem[i].address, mainMem[i].data);
+  }
+}
 
 int main() {
-  PageTableEntry pt[NUM_PTE];
-  MainMemoryEntry mainMem[NUM_ADDR_MM];
-  int i;
+  PageTableEntry pt[NUM_PTE];             // page table
+  MainMemoryEntry mainMem[NUM_ADDR_MM];   // main memory
+  bool end = false;                       // flag for ending shell
+  char* *args = NULL;                     // user input
+  int i;                                  // counter variable
 
-  // set up page table
+  // initialize page table
   for (i = 0; i < NUM_PTE; ++i) {
     pt[i].entry = i;
     pt[i].valid = 0;
@@ -38,11 +81,7 @@ int main() {
     pt[i].pageNum = i;
   }
 
-  // for (i = 0; i < NUM_PTE; ++i) {
-  //   printf("%d:%d:%d:%d\n", pt[i].entry, pt[i].valid, pt[i].dirty, pt[i].pageNum);
-  // }
-
-  // set up main memory
+  // initialize main memory
   int page = 0;
   for (i = 0; i < NUM_ADDR_MM; ++i) {
     mainMem[i].page = i % 2 == 1 ? page++ : page;
@@ -50,25 +89,36 @@ int main() {
     mainMem[i].data = -1;
   }
 
-  // for (i = 0; i < NUM_ADDR_MM; ++i) {
-  //   printf("%i:%i:%i\n", mainMem[i].page, mainMem[i].address, mainMem[i].data);
-
-  bool end = false;
-  char input[MAX_CHAR];
-  char** line[MAX_ARGS];
   while (!end) {
     // print prompt
     printf("> ");
-    fgets(input, sizeof(input) - 1, stdin);
-    line[0] = strtok(input, " \n");
-    for (i = 1; i < MAX_ARGS; ++i) {
-      line[i] = strtok(NULL, " \n");
-    }
+    args = parse();
 
-    for (i = 0; i < sizeof(line)/sizeof(line[0]); ++i) {
-      printf("%s ", line[i]);
+    // continue if empty line, otherwise execute according to command
+    if (args == NULL) {
+      continue;
     }
-    printf("\n");
+    else if (strcmp(args[0], "quit") == 0) {
+      end = true;
+    }
+    else {
+      if (strcmp(args[0], "read") == 0) {
+
+      }
+      else if (strcmp(args[0], "write") == 0) {
+
+      }
+      else if (strcmp(args[0], "showmain") == 0) {
+
+      }
+      else if (strcmp(args[0], "showdisk") == 0) {
+        
+      }
+      else if (strcmp(args[0], "showptable") == 0) {
+        showPTable(pt);
+      }
+    }
   }
+
   return 0;
 }
